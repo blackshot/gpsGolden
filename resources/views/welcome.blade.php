6b4b7a5aa741678c676d746d07396e4e70425bd2
Mauri Vergara <maurivergara247@gmail.com>
2021-05-24 23:11:09 -0400

    Se instala bootstrap

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,9 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <title>GPS Golden</title>
 </head>
 <body>
-    <h1>Hola Scarlett</h1>
+    <button type="button" class="btn btn-primary">Primary</button>
+
+    
 </body>
 </html>
