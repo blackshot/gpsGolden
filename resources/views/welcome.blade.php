47569a47ea1b66c92e1c10f2ad0c2ec9be99d2be
Mauri Vergara <maurivergara247@gmail.com>
2021-05-26 00:30:51 -0400

    Se inicia pagina principal

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,10 +4,93 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <title>GPS Golden</title>
 </head>
 <body>
-    
+    <section class="header">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark fondo-nav">
+            <div class="container-fluid">
+                <a class="navbar-brand texto-nav" href="/">GPS Golden</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarText">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="#">Servicios</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Contactanos</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col d-flex justify-content-center">
+                    <a href="" class="btn btn-warning">Contactanos</a>
+                </div>
+            </div>
+        </div>        
+    </section>
+
+
+    <section class="servicios">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <h1 class="text-center pt-5 mt-5 titulo-servicios ">Nuestros Servicios</h1>
+                </div>
+            </div>
+            <div class="row mt-5 pt-5 pb-5 text-center">
+                <div class="col-md-4 col">
+                    <div class="card" style="width: 18rem;">
+                        <img src="../../public/images/camion.png" class="card-img-top" alt="...">
+                        <div class="card-title">
+                            <h5>Servicio 1</h5>
+                        </div>
+                        <div class="card-body">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 col">
+                    <div class="card" style="width: 18rem;">
+                        <img src="../../public/images/camion.png" class="card-img-top" alt="...">
+                        <div class="card-title">
+                            <h5>Servicio 2</h5>
+                        </div>
+                        <div class="card-body">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 col">
+                    <div class="card" style="width: 18rem;">
+                        <img src="../../public/images/camion.png" class="card-img-top" alt="...">
+                        <div class="card-title">
+                            <h5>Servicio 3</h5>
+                        </div>
+                        <div class="card-body">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <section class="footer">
+        <h1 class="pt-5 pb-5">Redes sociales</h1>
+    </section>
+
 </body>
 </html>
+
+
