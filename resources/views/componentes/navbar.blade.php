bb563a0495af3a38a5601166ab62d2ba42afcbc1
Hector Echiburu <black.shot@live.cl>
2021-06-01 14:38:10 -0400

    Ordenado todo el layout eliminada la basura sobrante
    haz composer install para instalar la debugbar

---
+++ resources/views/componentes/navbar.blade.php
@@ -0,0 +1,34 @@
+<section class="header">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fondo-nav">
+        <div class="container-fluid">
+            <a class="navbar-brand texto-nav" href="/">GPS Golden</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarText">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="#">Servicios</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contáctanos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="https://www.qmgps.cl/gps_golden/inicioGOLDEN.aspx" target="_blank">Portal</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="mr-auto">
+                <a href="" class="btn btn-warning">Contáctanos</a>
+            </div>
+        </div>
+    </nav>
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col d-flex justify-content-center">
+                <a href="" class="btn btn-warning">Contáctanos</a>
+            </div>
+        </div>
+    </div>        
+</section>
